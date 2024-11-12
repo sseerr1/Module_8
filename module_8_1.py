021f37e68345425a654c239f82aa1c1bc8b2563d
sseerr1 <46776271+sseerr1@users.noreply.github.com>
2024-11-12 18:28:31 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,11 +1,10 @@
 def add_everything_up(a: object, b):
     try:
         return a + b
-    except:
+    except TypeError:
         return str(a) + str(b)
 
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
-
