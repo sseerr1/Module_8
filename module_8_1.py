b73264d744fd74419fc0ebe46e4860d49f8b8bd1
sseerr1 <46776271+sseerr1@users.noreply.github.com>
2024-11-12 18:21:36 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -2,10 +2,10 @@ def add_everything_up(a: object, b):
     try:
         return a + b
     except:
-        return str(a)+str(b)
-
+        return str(a) + str(b)
 
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
+
